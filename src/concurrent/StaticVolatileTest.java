5ae7f9810350198a705e3c73a85e8e3d24f25b8c
omottec <omottec@gmail.com>
2016-06-29 12:07:29 +0800

    VolatileTest for static var

---
+++ src/concurrent/StaticVolatileTest.java
@@ -0,0 +1,38 @@
+package concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by qinbingbing on 6/29/16.
+ */
+public class StaticVolatileTest {
+    public static final int THREAD_NUM = 1000;
+    public static volatile int count = 0;
+    private static CountDownLatch downLatch = new CountDownLatch(THREAD_NUM);
+
+    public static void inc() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count++;
+        downLatch.countDown();
+        System.out.println(Thread.currentThread());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < THREAD_NUM; i++)
+            new Thread() {
+                @Override
+                public void run() {
+                    inc();
+                }
+            }.start();
+        downLatch.await();
+        System.out.println("count:" + count);
+    }
+}

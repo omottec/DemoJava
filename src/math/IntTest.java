cb1a041bec7cd1509c5f547e47abb4d569ef942b
omottec <omottec@gmail.com>
2016-08-31 10:17:50 +0800

    IntTest

---
+++ src/math/IntTest.java
@@ -0,0 +1,12 @@
+package math;
+
+/**
+ * Created by qinbingbing on 7/28/16.
+ */
+public class IntTest {
+    public static void main(String[] args) {
+        int x = 10000;
+//        int y = 10_000;
+
+    }
+}

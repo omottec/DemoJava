44370bcc0b7627284fa966bcaa5d546df8815a42
omottec <omottec@gmail.com>
2016-09-26 12:11:08 +0800

    add operator pkg

---
+++ src/operator/OperatorTest.java
@@ -0,0 +1,13 @@
+package operator;
+
+/**
+ * Created by qinbingbing on 9/26/16.
+ */
+public class OperatorTest {
+    public static void main(String[] args) {
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ false);
+        System.out.println(false ^ true);
+    }
+}

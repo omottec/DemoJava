1991fea17e24a247384865912c193c8991b834ec
omottec <omottec@gmail.com>
2016-10-11 15:25:02 +0800

    assert test

---
+++ src/control/AssertTest.java
@@ -0,0 +1,12 @@
+package control;
+
+/**
+ * Created by qinbingbing on 10/11/16.
+ * ClassLoader
+ * java -ea[-da] JavaClass
+ */
+public class AssertTest {
+    public static void main(String[] args) {
+        assert false : "err occur";
+    }
+}

da62b69e7bf553715c2ab57fa22d9dc44c9700da
omottec <omottec@gmail.com>
2016-10-20 14:07:36 +0800

    test javac

---
+++ src/machanism/RefClass.java
@@ -0,0 +1,11 @@
+package machanism;
+
+/**
+ * Created by qinbingbing on 10/20/16.
+ */
+public class RefClass {
+    public static void main(String[] args) {
+        ReferencedClass referencedClass = new ReferencedClass();
+        referencedClass.i = 1;
+    }
+}
---
+++ src/machanism/ReferencedClass.java
@@ -0,0 +1,8 @@
+package machanism;
+
+/**
+ * Created by qinbingbing on 10/20/16.
+ */
+public class ReferencedClass {
+    public int i;
+}

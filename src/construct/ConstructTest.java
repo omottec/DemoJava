5e853ce2778406ecccf7e76d9d53b5de75198bbd
omottec <omottec@gmail.com>
2016-12-20 15:37:34 +0800

    add ConstructTest

---
+++ src/construct/ConstructTest.java
@@ -0,0 +1,14 @@
+package construct;
+
+/**
+ * Created by qinbingbing on 12/20/16.
+ */
+public class ConstructTest {
+    public static void main(String[] args) {
+//        new View();
+    }
+}
+
+abstract class View {
+    abstract void measure(int width, int height);
+}

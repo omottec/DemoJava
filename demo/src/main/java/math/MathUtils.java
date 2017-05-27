3347c645f27eb4c8202d3492e7af7f87da933be7
omottec <omottec@gmail.com>
2017-05-27 15:10:58 +0800

    java.math.BigDecimal#stripTrailingZeros

--- demo/src/main/java/math/MathUtils.java
+++ demo/src/main/java/math/MathUtils.java
@@ -8,8 +8,11 @@ public final class MathUtils {
 
     public static String getTrimmedValue(double d) {
         String s = String.valueOf(d);
-        while (!"".equals(s) && (s.charAt(s.length() - 1) == '0' || s.charAt(s.length() - 1) == '.'))
-            s = s.substring(0, s.length() - 1);
+        if (!s.contains(".")) return s;
+        while (s.charAt(s.length()-1) == '0')
+            s = s.substring(0, s.length()-1);
+        if (s.charAt(s.length()-1) == '.')
+            return s.substring(0, s.length() - 1);
         if ("".equals(s)) return String.valueOf("0");
         return s;
     }
--- test/src/test/java/math/MathTest.java
+++ test/src/test/java/math/MathTest.java
@@ -32,10 +32,42 @@ public class MathTest extends TestCase {
 
     private String getTrimmedValue(double d) {
         String s = String.valueOf(d);
-        while (!"".equals(s) && (s.charAt(s.length() - 1) == '0' || s.charAt(s.length() - 1) == '.'))
-            s = s.substring(0, s.length() - 1);
+        if (!s.contains(".")) return s;
+        while (s.charAt(s.length()-1) == '0')
+            s = s.substring(0, s.length()-1);
+        if (s.charAt(s.length()-1) == '.')
+            return s.substring(0, s.length() - 1);
         if ("".equals(s)) return String.valueOf("0");
         return s;
     }
 
+    @Test
+    public void testMinus() {
+        System.out.println(125 - 123.00);
+        BigDecimal bigDecimal = BigDecimal.valueOf(125).subtract(new BigDecimal("123.00"));
+        System.out.println("bigDecimal" + bigDecimal);
+        System.out.println("bigDecimal.doubleValue:" + bigDecimal.doubleValue());
+        System.out.println("bigDecimal.stripTrailingZeros:" + bigDecimal.stripTrailingZeros());
+        System.out.println("getTrimmedValue(bigDecimal.doubleValue()):" + getTrimmedValue(bigDecimal.doubleValue()));
+    }
+
+    @Test
+    public void testTrimmedValue() {
+        System.out.println(getTrimmedValue(.30));
+        System.out.println(getTrimmedValue(.00));
+        System.out.println(getTrimmedValue(0.1));
+        System.out.println(getTrimmedValue(0.0));
+        System.out.println(getTrimmedValue(.1));
+        System.out.println(getTrimmedValue(.0));
+    }
+
+    @Test
+    public void testStripTrailingZeros() {
+        System.out.println(new BigDecimal(".30").stripTrailingZeros());
+        System.out.println(new BigDecimal(".00").stripTrailingZeros());
+        System.out.println(new BigDecimal("0.1").stripTrailingZeros());
+        System.out.println(new BigDecimal("0.0").stripTrailingZeros());
+        System.out.println(new BigDecimal(".1").stripTrailingZeros());
+        System.out.println(new BigDecimal(".0").stripTrailingZeros());
+    }
 }

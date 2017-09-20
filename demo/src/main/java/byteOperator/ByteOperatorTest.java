8316858552b2753661a318e2a47ca950ca639328
omottec <omottec@gmail.com>
2017-09-20 13:31:08 +0800

    test Integer.toBinaryString

---
+++ demo/src/main/java/byteOperator/ByteOperatorTest.java
@@ -0,0 +1,15 @@
+package byteOperator;
+
+/**
+ * Created by qinbingbing on 20/09/2017.
+ */
+public class ByteOperatorTest {
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        System.out.println(Integer.toBinaryString(-2));
+        System.out.println(Integer.toBinaryString(2));
+    }
+}

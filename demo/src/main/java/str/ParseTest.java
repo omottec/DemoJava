ebb5ccc4c51f667efe150485f5e74122dc0158b8
omottec <omottec@gmail.com>
2018-02-07 15:42:59 +0800

    test NumberFormatException

---
+++ demo/src/main/java/str/ParseTest.java
@@ -0,0 +1,10 @@
+package str;
+
+/**
+ * Created by qinbingbing on 07/02/2018.
+ */
+public class ParseTest {
+    public static void main(String[] args) {
+        long l = Long.parseLong("0");
+    }
+}

39f69df3d3bf92314b69dc7e82d978a7e6c94f10
omottec <omottec@gmail.com>
2018-11-20 17:36:46 +0800

    negative num

--- demo/src/main/java/math/IntTest.java
+++ demo/src/main/java/math/IntTest.java
@@ -8,5 +8,9 @@ public class IntTest {
         int x = 10000;
 //        int y = 10_000;
 
+        System.out.println("" + 0x80000000);
+        System.out.println("" + 0x80000001);
+        System.out.println("" + 0x8fffffff);
+        System.out.println("" + 0xffffffff);
     }
 }

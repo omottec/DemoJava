3b3e2b933c9bfae45e41e9eaf7ffdde040910ced
omottec <omottec@gmail.com>
2018-12-19 16:57:51 +0800

    test default value

--- demo/src/main/java/clazz/ClazzTest.java
+++ demo/src/main/java/clazz/ClazzTest.java
@@ -3,6 +3,7 @@ package clazz;
 import model.Dog;
 import model.Person;
 import model.Teacher;
+import ref.ReferentObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +18,8 @@ import java.util.concurrent.ConcurrentMap;
 public class ClazzTest {
     public static void main(String[] args) {
 //        testAssignable();
-        testAsSubclass();
+//        testAsSubclass();
+        testDefaultValue();
     }
 
     private static void testAssignable() {
@@ -36,6 +38,29 @@ public class ClazzTest {
         try {
             Object o = clazz.newInstance();
             System.out.println(o);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testDefaultValue() {
+        ReflectObject reflectObject = new ReflectObject();
+        System.out.println("reflectObject:" + reflectObject);
+        try {
+            ReflectObject reflectObject1 = (ReflectObject) Class.forName("clazz.ReflectObject").newInstance();
+            System.out.println("reflectObject1:" + reflectObject1);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            ReflectObject reflectObject2 = ReflectObject.class.newInstance();
+            System.out.println("reflectObject2:" + reflectObject2);
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
---
+++ demo/src/main/java/clazz/ReflectObject.java
@@ -0,0 +1,13 @@
+package clazz;
+
+/**
+ * Created by qinbingbing on 19/12/2018.
+ */
+public class ReflectObject {
+    public int intFiled = -1;
+
+    @Override
+    public String toString() {
+        return super.toString() + "|intFiled:" + intFiled;
+    }
+}

9ca5b3d580d0d6de14e793f6761213048033ad12
qinbingbing <qinbingbing@bytedance.com>
2020-06-11 22:00:56 +0800

    test && ||

--- demo/src/main/java/operator/OperatorTest.java
+++ demo/src/main/java/operator/OperatorTest.java
@@ -5,9 +5,22 @@ package operator;
  */
 public class OperatorTest {
     public static void main(String[] args) {
+        test2();
+    }
+
+    private static void test1() {
         System.out.println(true ^ true);
         System.out.println(true ^ false);
         System.out.println(false ^ false);
         System.out.println(false ^ true);
+    }
+
+    private static void test2() {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = false;
+        System.out.println(b1 || b2 && b3);
+        System.out.println((b1 || b2) && b3);
+        System.out.println(b1 || (b2 && b3));
     }
 }
